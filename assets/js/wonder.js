8953b23a63080f4c67b5457fcebf587f3e92ffb2
summernalie <samanalie@outlook.com>
2017-05-30 20:46:17 +0530

    W Effects

--- assets/js/wonder.js
+++ assets/js/wonder.js
@@ -1,5 +1,25 @@
 $(document).ready(function () {
     setTimeout(function () {
-        $(".logo-contain").addClass('fadeout');
+      //  $(".logo-contain").addClass('fadeout');
     }, 2000);
+    //window.sr = new scrollReveal();
+    //sr.reveal('#intro h1', { duration: 200, origin: 'bottom', easing: 'ease-in-out' })
+    
+    window.addEventListener('scroll', function(e){
+            var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+                shrinkOn = 50;
+                
+            if (distanceY > shrinkOn) {
+                $(".logo-contain").removeClass('fadebackin');
+                $(".logo-contain").addClass('fadeout');
+                
+                $("#intro h1").addClass('fadein');
+            } else {
+                 
+                
+                $("#intro h1").removeClass('fadein');
+                $(".logo-contain").addClass('fadebackin');
+                
+            }
+        });
 });
