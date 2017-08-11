45a90bff4c813215382cc06ffe5564072c8a78b4
samanalie <samanaliej@itelasoft.com>
2017-08-11 18:27:03 +0530

    genius

---
+++ assets/js/genius.js
@@ -0,0 +1,24 @@
+jQuery(function ($) {
+    $(".content").click(function () {
+        $(this).addClass("active");
+        //console.log($(this).data("pop")); 
+        $("#pen").addClass("open");
+        $("#pen .inner").html($(this).data("pop"))
+    });
+
+    $("#pen #close").click(function () {
+        $(".content.active").removeClass("active");
+        $("#pen").removeClass("open");
+        //$("#pen .inner").html('');
+    });
+
+
+    $(document).mouseup(function (e) {
+        var container = $("#pen");
+
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            container.removeClass("open");
+        }
+    });
+});
