1e0ee7228ed680aa3754f951fa073406a6491503
summernalie <samanalie@outlook.com>
2017-09-11 22:39:29 +0530

    final genius

--- assets/js/genius.js
+++ assets/js/genius.js
@@ -1,17 +1,28 @@
+var pos;
 jQuery(function ($) {
     $(".content").click(function () {
         $(".content.active").removeClass("active");
         $(this).addClass("active");
         //console.log($(this).data("pop")); 
         $("#pen").addClass("open");
-        $("#pen .inner").html($(this).data("pop"))
+        $("#pen .inner #meaning").html($(this).data("pop"));
+        $("#pen").css('height', $('#lyrics').outerHeight());
+        pos = $(this).offset().top;
+
+        if (pos < 800) {
+            $('.annotation_sidebar_arrow').css('margin-top', pos - 350);
+            $('#pen .inner').css('margin-top', pos - 400);
+        } else {
+            $('.annotation_sidebar_arrow').css('margin-top', pos - 350);
+            $('#pen .inner').css('margin-top', pos - 450);
+        }
     });
 
     $("#pen #close").click(function (e) {
         $(".content.active").removeClass("active");
         $("#pen").removeClass("open");
         //$("#pen .inner").html('');
-       // e.stopPropagation();
+        // e.stopPropagation();
     });
 
 
@@ -21,6 +32,8 @@ jQuery(function ($) {
         // if the target of the click isn't the container nor a descendant of the container
         if (!container.is(e.target) && container.has(e.target).length === 0) {
             container.removeClass("open");
+
+            $(".content.active").removeClass("active");
         }
     });
 });
